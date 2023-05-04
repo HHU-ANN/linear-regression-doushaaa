ff036cea5599e1d00f910aacbdab98b4f752c588
= <1162057351@qq.com>
2023-05-04 21:40:46 +0800

    第一次提交

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -2,6 +2,9 @@
 
 import os
 
+
+
+
 try:
     import numpy as np
 except ImportError as e:
@@ -9,12 +12,32 @@ except ImportError as e:
     import numpy as np
 
 def ridge(data):
-    pass
-    
+    a1=0.00173
+    x,y=read_data()
+    T=np.eye(x.transpose().dot(x).shape[1])
+    w=np.linalg.inv(x.transpose().dot(x)+a1*T).dot(x.transpose()).dot(y)
+    return(data.dot(w))
+
+
 def lasso(data):
-    pass
+   x,y=read_data()
+   w=np.zeros((x.shape[1],1))
+
+   a = 0.00000000009299999999999999999
+   print(a)
+   for i in range(15):
+    dw=x.transpose().dot(x.dot(w)-y)-a*np.sign(w)
+    w=w-a*dw
+   ypre=data.dot(w)
+   print(ypre)
+   sum=0
+   average=np.mean(ypre)
+   return average
+
 
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
     y = np.load(path + 'y_train.npy')
     return x, y
+
+
